957dbe8fc0459de49de15820b9800af93599c26f
Oskar Tengberg <52031625+MrTengil@users.noreply.github.com>
2020-10-22 23:27:39 +0200

    Chatt room server

--- client.py
+++ client.py
@@ -1,6 +1,28 @@
 import socket
+import pickle
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1234))
 
-msg = s.recv(1024)
-print(msg.decode("utf-8"))
+HEADERSIZE = 10
+
+while True:
+    full_msg = b''
+    new_msg = True
+    while True:
+        msg = s.recv(16)
+        if new_msg:
+            print(f"new message length: {msg[:HEADERSIZE]}")
+            msglen = int(msg[:HEADERSIZE])
+            new_msg = False
+
+        full_msg += msg
+
+        if len(full_msg)-HEADERSIZE==msglen:
+            print("full msg recvd")
+            print(full_msg[HEADERSIZE:])
+
+            d = pickle.loads(full_msg[HEADERSIZE:])
+            print(d)
+            new_msg = True
+            full_msg = 'b'
+    print(full_msg)
--- server.py
+++ server.py
@@ -1,10 +1,23 @@
 import socket
+import pickle
+
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1234))
 s.listen(5)
+HEADERSIZE = 10
 
 while True:
     clientsocket, address = s.accept()
     print(f"Connection from {address} has been established!")
-    clientsocket.send(bytes("Welcome to the server!", "utf-8"))
+
+    d = {
+        1: "Hey",
+        2: "There"
+    }
+    msg = pickle.dumps(d)
+
+    msg = bytes(f'{len(msg):<{HEADERSIZE}}', "utf-8") + msg
+
+    clientsocket.send(msg)
